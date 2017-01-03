2b53cce012b9429155e0276daea566801026fb4f
Sohaib Nehal <sohaib.nehal@ymail.com>
2017-01-03 13:11:21 +0500

    Fixing the initial load issue

--- scripts/app.js
+++ scripts/app.js
@@ -33,38 +33,24 @@ var app = angular
 
 app.config(['$translateProvider', function ($translateProvider) {
 
-/*  $translateProvider.useStaticFilesLoader({
+  $translateProvider.useStaticFilesLoader({
     prefix: 'scripts/translations/',
     suffix: '.json'
-  });*/
+  });
 
-/*  $translateProvider.registerAvailableLanguageKeys(['en_us', 'en_uk', 'de'], {
+  $translateProvider.registerAvailableLanguageKeys(['en_us', 'en_uk', 'de'], {
     'en_US': 'en_us',
     'en':'en_us',
     'en_UK': 'en_uk',
     'de_DE': 'de',
     'de_CH': 'de'
-  });*/
-/*
+  });
+
+  $translateProvider.fallbackLanguage('en_us');
   $translateProvider.determinePreferredLanguage();
 
-  $translateProvider.fallbackLanguage('en_us');*/
 
 
-  $translateProvider
-    .useStaticFilesLoader({
-      prefix: 'scripts/translations/',
-      suffix: '.json'
-    })
-    .registerAvailableLanguageKeys(['en_us', 'en_uk', 'de'], {
-      'en_US': 'en_us',
-      'en':'en_us',
-      'en_UK': 'en_uk',
-      'de_DE': 'de',
-      'de_CH': 'de'
-    })
-    .fallbackLanguage('en_us')
-    .determinePreferredLanguage();
 
 
 
